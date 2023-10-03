c78b2daef18f8190c2dde99854c464862d7d2169
AbdiazizSaman <farhanadam394@gmail.com>
2023-10-03 10:22:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 // Add your code here
- 
-function submitData(name,email) {
+
+function submitData(name, email) {
   // Create an object with the data you want to send in the POST request
   const postData = {
     name: name,
     email: email,
   };
 
-  const postRequest = {
-
+  // Create the fetch POST request
+  fetch("http://localhost:3000/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
     body: JSON.stringify(postData),
-}
-  // Create the fetch POST request
-  fetch("http://localhost:3000/users", postRequest)
-   
-  .then((resp) => resp.json())
+  })
+    .then((resp) => resp.json())
     .then((data) => {
       // The response should contain the new ID value
       const newId = data.id;
 
-      const idElement = document.createElement('p');
+      const idElement = document.createElement("p");
       idElement.textContent = `Unauthorized Access${newId}`;
       document.body.appendChild(idElement);
     })
-   
+
     .catch((error) => {
       // Handle the error and append the error message to the DOM
-      const errorMessage = document.createElement('div');
-      errorMessage.textContent = 'Error: ' + error.message;
+      const errorMessage = document.createElement("div");
+      errorMessage.textContent = "Error: " + error.message;
 
-      const errorContainer = document.getElementById('error-container');
+      const errorContainer = document.getElementById("error-container");
       errorContainer.appendChild(errorMessage);
 
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
